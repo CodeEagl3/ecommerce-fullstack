a8d52c68afb2320a48f1ca4e0de79e4d2ad7d27d
CodeEagl3 <oluwasomidotun7@gmail.com>
2021-02-22 16:26:07 +0100

    magiclink added and authentication

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,7 +1,11 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
+import { Magic } from "magic-sdk";
+import { MAGIC_PUBLIC_KEY } from "../utils/urls";
 
 const AuthContext = createContext()
+
+let magic;
 
 export const AuthProvider = (props) => {
 
@@ -14,8 +18,13 @@ export const AuthProvider = (props) => {
      */
 
     const loginUser = async (email) => {
-        setUser({ email })
-        router.push("/")
+        try{
+            await magic.auth.loginWithMagicLink({ email })
+            setUser({ email })
+            router.push("/")
+        } catch (errors) {
+            setUser(null)
+        }
     }
 
     /**
@@ -23,9 +32,35 @@ export const AuthProvider = (props) => {
      */
 
     const logoutUser = async () => {
-        setUser(null)
-        router.push("/")
+        try {
+            await magic.user.logout()
+            setUser(null)
+            router.push("/")
+        }
+        catch (errors) {
+
+        }
     }
+
+    const checkUserLoggedIn = async () => {
+        try {
+            const isLoggedIn = await magic.user.isLoggedIn()
+
+            if(isLoggedIn) {
+                const { email } = await magic.user.getMetadata()
+                setUser({email})
+            }
+        }
+        catch (err) {
+
+        }
+    }
+
+    useEffect(() => {
+        magic = new Magic(MAGIC_PUBLIC_KEY)
+
+        checkUserLoggedIn
+    }, [])
 
     return (
         <AuthContext.Provider value={{ user, loginUser, logoutUser}}>
--- utils/urls.js
+++ utils/urls.js
@@ -1,5 +1,5 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337"
-
+export const MAGIC_PUBLIC_KEY = process.env.NEXT_PUBLIC_MAGIC_API_URL || 'pk_test_D4FCE13FBF68995F'
 /**
  * Given an image return the Url
  * Works for local and deployed strapis
